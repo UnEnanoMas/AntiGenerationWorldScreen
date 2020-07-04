783a038f080269588ca7a3895618efa654a135f0
matymare <57559400+matymare@users.noreply.github.com>
2020-07-04 13:03:27 +0200

    Update AGS.php

--- src/matymare/AntiGeneratingWorldScreen/AGS.php
+++ src/matymare/AntiGeneratingWorldScreen/AGS.php
@@ -1,1 +1,28 @@
+<?php
+
+namespace matymare\AntiGeneratingWorldScreen;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+class AGS extends PluginBase implements Listener{
+    
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+    }
+    
+    public function onDamage(EntityDamageEvent $event) {
+$player = $event->getEntity();
+if($event->getFinalDamage() >= $player->getHealth()) {
+$event->setCancelled();
+$player->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
+$player->setHealth($player->getMaxHealth());
+   }
+}
+
+    public function onDisable(){
+    }
+}
 
